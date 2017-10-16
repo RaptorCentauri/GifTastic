dee54bc35d4dffd699aa6ab759529f8a5c3d313b
RaptorCentauri <dave.leo.shilander@icloud.com>
2017-10-16 09:53:09 -0500

    updated files

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,17 +1,45 @@
+// Variable Declarations
 var topics = ["Rugrats", "Ducktales", `Dark Wing Duck`, "Hey Arnold", "Ned's Newt", "Tailspin", "Digimon"];
 var stillGif;
 var animateGif;
+var results;
 
 
-for (i=0; i<topics.length; i++){
-	$(".button-box").append(`<button class=topicSearch data-name="${topics[i]}">${topics[i]}</button>`);
-	// var searchQuery = $(".topicSearch").data(`name`);
+//Function Declarations
+function getGif(search){
+
+
+
+	$.get("https://api.giphy.com/v1/gifs/search?", {api_key:"inMvyNCFgyQC1dyf6cQvHVk3djujZ24O", q:`${search}`, limit:"10"}).done(function(response){
+
+
+	results = response.data;
+	console.log(results);
+
+
+	for(i=0; i<10; i++){
+
+		stillGif = results[i].images.fixed_height_still.url;
+		animateGif = results[i].images.fixed_height.url;
+		rating = results[i].rating;
+
+		$(".results-box").append(`<div class=gif-box><img src=${stillGif} data-still=${stillGif} data-animate=${animateGif} data-state=still class=gif><div class=rating>Rated: ${rating}</div></div>`);
+
+		// $(".results-box").append(`<div class=rating>Rated: ${rating}</div>`);
+	
+	}
+
+	});
 }
 
 
-var results;
-// var searchQuery;
 
+for (i=0; i<topics.length; i++){
+
+	$(".button-box").append(`<button class=topicSearch data-name="${topics[i]}">${topics[i]}</button>`);
+}
+
+//Click Event for Mobile Menu
 $(document.body).on("click", ".hamburger-button", function(){
 
 	if($(".button-box").css(`visibility`) == `hidden`){
@@ -27,11 +55,9 @@ $(document.body).on("click", ".hamburger-button", function(){
 	// else if($(".button-box").css(`visibility`) == `visible`){
 	// 	$(".button-box").css(`visibility`,`hidden`);
 	// }
-
-
-
 })
 
+//Click Event for Adding Search
 $(".search-button").on("click", function(){
 	event.preventDefault();
 	seachValue = $("#gif-search").val().trim();
@@ -42,26 +68,17 @@ $(".search-button").on("click", function(){
 
 	$(".button-box").append(`<button class=topicSearch data-name="${seachValue}">${seachValue}</button>`);
 	// var searchQuery = $(".topicSearch").data(`name`);
-
-
 })
 
-
+//Click Event to Search Giphy
 $(document.body).on("click", ".topicSearch", function(){
 
-
 	$(".results-box").empty();
-
 	searchQuery = $(this).data(`name`);
-
-	console.log(`You searched Giphy for: ${searchQuery}`);
-
-
-	getGif(searchQuery);
-	
+	getGif(searchQuery);	
 })
 
-
+//Click Event to Play/Pause Gif
 $(document.body).on("click", ".gif", function(){
 
 
@@ -81,31 +98,9 @@ $(document.body).on("click", ".gif", function(){
 	if(state === 'animate'){
 		$(this).attr("src", $(this).attr('data-still'));
 		$(this).attr('data-state', 'still');   
-	}
-	
+	}	
 })
 
 
 
-function getGif(search){
 
-
-
-	$.get("https://api.giphy.com/v1/gifs/search?", {api_key:"inMvyNCFgyQC1dyf6cQvHVk3djujZ24O", q:`${search}`, limit:"10"}).done(function(response){
-
-
-	results = response.data;
-
-	for(i=0; i<10; i++){
-
-		stillGif = results[i].images.fixed_height_still.url;
-		animateGif = results[i].images.fixed_height.url;
-
-		$(".results-box").append(`<img src=${stillGif} data-still=${stillGif} data-animate=${animateGif} data-state=still class=gif>`);
-		// $(".results-box").append(results[i].rating);
-		// console.log(`Search: ${searchQuery}`);
-		
-	}
-
-	});
-}
